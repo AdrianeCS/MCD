5b0e84d86687559af83d31116e5daa86299264ed
adriane <adrianedacosta21@gmail.com>
2022-05-20 22:00:11 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int dividendo, divisor, resto;
+
+Console.WriteLine("máximo divisor comun (método iterativo\n");
+
+Console.Write("digite o primeiro numero (a)...: ");
+dividendo = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("digite o segundo numero (b)...: ");
+divisor = Convert.ToInt32(Console.ReadLine());
+
+do
+{
+    resto = dividendo % divisor;
+    dividendo = divisor;
+    divisor = divisor = resto;
+
+} while (resto != 0);
+
+Console.WriteLine($"MDC (a,b) = {dividendo}");
